18512af852fc6cd43ce92fb5f301caac3a990d59
v-i-k-a-s-1-7 <vikasrajak2003@gmail.com>
2024-10-15 01:40:12 +0530

    for noob programmers

--- track_resources.sh
+++ track_resources.sh
@@ -2,15 +2,23 @@
 
 set -x
 
-# AWS S3
+## AWS S3
+
+# this will list all the s3 buckets available. 
 aws s3 ls
 
-# AWS EC2
+## AWS EC2
+
+# this will list all the EC2 instance ids.
 aws ec2 describe-instances | jq ".Reservations[].Instances[].InstanceId"
 
-# AWS lambda
+## AWS lambda
+
+# this will list all the lambda functions available.
 aws lambda list-functions
 
-# AWS IAM users
+## AWS IAM users
+
+# this will list all the iam users.
 aws iam list-users
 
